0b57f7027e144d0d9ce4f33932790f17885f626e
Tony Bowden <tony@tmtm.com>
2017-01-13 14:59:11 +0000

    Rewrite using Scraped

--- scraper.rb
+++ scraper.rb
@@ -10,38 +10,85 @@ require 'scraperwiki'
 # OpenURI::Cache.cache_path = '.cache'
 require 'scraped_page_archive/open-uri'
 
-def noko_for(url)
-  Nokogiri::HTML(open(url).read)
-end
-
-def scrape_mp(url)
-  noko = noko_for(url)
-  image = noko.css('div#story img[src*="/cv/"]/@src').text
-  image = URI.join(url, image) unless image.to_s.empty?
-  {
-    name:   noko.css('form#myform h2').text.tidy,
-    image:  image.to_s,
-    source: url.to_s,
-  }
-end
-
-def scrape_term(termid, url)
-  noko = noko_for(url)
-  noko.css('div#story div#story table').first.css('tr').each do |tr|
-    tds = tr.css('td')
-    mp_page = URI.join url, tds[0].css('a/@href').text
-    data = {
-      id:        mp_page.to_s[/key=(\w+)/, 1],
-      sort_name: tds[0].text.tidy,
-      party:     tds[1].text.tidy,
-      email:     tds[2].css('a').text.tidy.reverse,
-      website:   tds[3].css('a/@href').text,
-      term:      termid,
-    }.merge(scrape_mp(mp_page))
-    # puts data
-    ScraperWiki.save_sqlite(%i(id term), data)
+# Fix emails in the format:
+#   <a class="mail" href="mailto:">eb.remaked@xkyul.retep</a>
+class ReversedEmails < Scraped::Response::Decorator
+  def body
+    Nokogiri::HTML(super).tap do |doc|
+      doc.css('a.mail[href*=mailto]').each do |a|
+        a[:href] = 'mailto:' + a.text.reverse
+      end
+    end.to_s
   end
 end
 
+class LaChambre
+  class HTML < Scraped::HTML
+    decorator ReversedEmails
+    decorator Scraped::Response::Decorator::AbsoluteUrls
+  end
+
+  class MemberPage < HTML
+    field :name do
+      noko.css('form#myform h2').text.tidy
+    end
+
+    field :image do
+      noko.css('div#story img[src*="/cv/"]/@src').text
+    end
+  end
+
+  class MembersPage < HTML
+    field :members do
+      noko.css('div#story div#story table').first.css('tr').map do |tr|
+        fragment tr => MemberRow
+      end
+    end
+  end
+
+  class MemberRow < Scraped::HTML
+    field :id do
+      source.to_s[/key=(\w+)/, 1]
+    end
+
+    field :sort_name do
+      tds[0].text.tidy
+    end
+
+    field :party do
+      tds[1].text.tidy
+    end
+
+    field :email do
+      tds[2].css('a/@href').text.gsub('mailto:', '')
+    end
+
+    field :website do
+      tds[3].css('a/@href').text
+    end
+
+    field :source do
+      tds[0].css('a/@href').text
+    end
+
+    private
+
+    def tds
+      noko.css('td')
+    end
+  end
+end
+
+def scrape(h)
+  url, klass = h.to_a.first
+  klass.new(response: Scraped::Request.new(url: url).response)
+end
+
+start = 'http://www.lachambre.be/kvvcr/showpage.cfm?section=/depute&language=fr&cfm=cvlist54.cfm?legis=54'
+data = scrape(start => LaChambre::MembersPage).members.map do |mem|
+  mem.to_h.merge(term: 54).merge(scrape(mem.source => LaChambre::MemberPage).to_h)
+end
+# puts data.map { |r| r.sort_by { |k, _| k }.to_h }
+
 ScraperWiki.sqliteexecute('DELETE FROM data') rescue nil
-scrape_term(54, 'http://www.lachambre.be/kvvcr/showpage.cfm?section=/depute&language=fr&cfm=cvlist54.cfm?legis=54')
+ScraperWiki.save_sqlite(%i(id term), data)
